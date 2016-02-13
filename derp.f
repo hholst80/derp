01d878ea2b49a0a8345636b4d1dc04894ac8d0f2
Henrik Holst <holst@matmech.com>
2016-02-13 22:03:55 +0100

    Backprop gradient works

--- derp.f
+++ derp.f
@@ -1,72 +1,122 @@
-C     TODO:
-C
-C     + Analytic gradient.
-C     + Back propagation.      
-C     + Back propagation
-C     + Batch gradient descent.
-      
+C     Naive implementation of backpropagation  to aid understanding of the principles.
+
       program derp
-        
+      implicit none
+
+      integer nx, nh, ny, ntrain
       parameter ( nx=2, nh=2, ny=1, ntrain=4 )
-      real x(nx), h(nh), y(ny)
-      real hw(nh,nx), yw(ny,nh)
-      real hg(nh,nx), yg(ny,nh)
+      real        x(nx), h(nh), y(ny)
+
+C     a(i,j): weight from node x(j) to node h(i).
+C     b(i,j): weight from node h(j) to node y(i).
+C     same convention as bprod.m in NNSYMID package (minus bias unit):
+C     http://www.iau.dtu.dk/research/control/nnsysid.html
+
+      real a(nh,nx), b(ny,nh)
+      real agrad(size(a,1),size(a,2)), bgrad(size(b,1),size(b,2))
+      real ydelta(ny), hdelta(nh)
       real xtrain(nx,ntrain), ytrain(ny,ntrain)
-      
-C     Setup training examples      
-      
+
+C     Setup training examples
+
       data xtrain / 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0 /
       data ytrain / 0.0, 1.0, 1.0, 0.0 /
-      
-      call random_number(hw)
-      call random_number(yw)
+
+      integer i, j, iter, niter
+      real    ecost
+      real    tdelta, t0, t1
+      parameter ( niter = 100000 )
+
+C     Initialize weights to something random.
+
+      print*,'=================================================='
+
+      print*, 'Initializing random weights.'
+
+      call random_number(a)
+      call random_number(b)
+
+      print*,'=================================================='
 
       call cost(ecost)
-      
-      iter = 0
-      
-      do while ( ecost > 0.1 )
-        print*, 'cost', ecost
-        print*, 'iter', iter
-        iter = iter + 1
+
+      call printmat('cost', reshape([ecost], [1,1]))
+
+      print*,'=================================================='
+
+      print*, 'Numerical gradient with central differences.'
+
+      agrad = 0.0
+      bgrad = 0.0
+
+      call ngrad()
+
+      call printmat('dE/da', agrad)
+      call printmat('dE/db', bgrad)
+
+      print*,'=================================================='
+
+      print*, 'Gradient with backpropagation.'
+
+      agrad = 0.0
+      bgrad = 0.0
+
+      call bpgrad()
+
+      call printmat('dE/da', agrad)
+      call printmat('dE/db', bgrad)
+
+      print*,'=================================================='
+
+      call cpu_time(t0)
+
+      do iter = 1, niter
+        call ngrad()
       end do
 
+      call cpu_time(t1)
+      tdelta = t1 - t0
+      print*, 'Central differences ', niter, ' iterations:', tdelta
+
+      call cpu_time(t0)
+
+      do iter = 1, niter
+        call bpgrad()
+      end do
+
+      call cpu_time(t1)
+      tdelta = t1 - t0
+      print*, 'Backpropagation ', niter, ' iterations:', tdelta
+
+      print*,'=================================================='
+
       contains
-        
-      elemental function sigmoid ( x )
-        intent(in) x
+
+      subroutine printmat ( name, smat )
+        character(len=*)  name
+        real              smat(:,:)
+        integer           i
+        print*, trim(name), ':'
+        do i = 1, size(smat,1)
+          print*, smat(i,:)
+        end do
+      end
+
+      real elemental function sigmoid ( x )
+        real, intent(in) :: x
         sigmoid = 1.0 / ( 1.0 + exp(-x) )
       end
-      
-      elemental function sigmoid_prime ( x )
-        intent(in) x
-        s = sigmoid(x)
-        sigmoid_prime = s * ( 1.0 - s)
-      end
-        
-      subroutine forward
-        h = matmul(hw,x)
-        h = sigmoid(h)  
-        y = matmul(yw,h)
-        y = sigmoid(y)
-      end
-      
-      subroutine analytic_gradient
-        delta(1,3) = y(1) - ytrain(
-C       real hg(nh,nx), yg(ny,nh)
-        yg(1,1) = delta(1,3) * sigmoid_prime(
+
+      subroutine forward ()
+        h = sigmoid(matmul(a,x))
+        y = sigmoid(matmul(b,h))
+C       h = matmul(a,x)
+C       y = matmul(b,h)
       end
 
-      subroutine backward 
-      end
-      
-      subroutine stocastic
-      end
-
-      subroutine batch
-      end
-      
       subroutine cost ( ecost )
+        real              ecost
+        integer           k
         ecost = 0.0
         do k = 1, ntrain
           x = xtrain(:,k)
@@ -74,5 +124,80 @@ C       real hg(nh,nx), yg(ny,nh)
           ecost = ecost + 0.5 * sum((y-ytrain(:,k))**2)
         end do
       end
-        
+
+C     compute a numerical approximation of the gradient
+
+      subroutine ngrad ()
+        real              delta, aij, bij, ep, en
+        integer           i, j
+        parameter       ( delta = sqrt(epsilon(1.0)) )
+
+        do j = 1, size(a,2)
+          do i = 1, size(a,1)
+            aij = a(i,j)
+            a(i,j) = aij + delta
+            call cost(ep)
+            a(i,j) = aij - delta
+            call cost(en)
+            agrad(i,j) = (ep-en)/(2*delta)
+            a(i,j) = aij
+          end do
+        end do
+
+        do j = 1, size(b,2)
+          do i = 1, size(b,1)
+            bij = b(i,j)
+            b(i,j) = bij + delta
+            call cost(ep)
+            b(i,j) = bij - delta
+            call cost(en)
+            bgrad(i,j) = (ep-en)/(2*delta)
+            b(i,j) = bij
+          end do
+        end do
+      end
+
+C     compute gradient with backpropagation NO UPDATE
+
+      subroutine bpgrad ()
+        integer           i, j, k
+        agrad = 0.0
+        bgrad = 0.0
+        do k = 1, ntrain
+          x = xtrain(:,k)
+          call forward
+          do i = 1, ny
+            ydelta(i) = ( y(i) - ytrain(i,k) ) * y(i) * ( 1.0 - y(i) )
+          end do
+C     compute dE/db
+          do j = 1, size(b,2)
+            do i = 1, size(b,1)
+              bgrad(i,j) = bgrad(i,j) + ydelta(i) * h(j)
+            end do
+          end do
+C     compute hdelta
+          hdelta = 0.0
+          do j = 1, nh
+            hdelta(j) = 0.0
+            do i = 1, ny
+              hdelta(j) = hdelta(j)
+     $                  + ydelta(i) * h(j) * ( 1.0 - h(j) ) * b(i,j)
+            end do
+          end do
+C     compute dE/da
+          do j = 1, size(a,2)
+            do i = 1, size(a,1)
+              agrad(i,j) = agrad(i,j) + hdelta(i) * x(j) ! <<< this is right
+            end do
+          end do
+C     end compute dE/da
+        end do
+      end
+
+      subroutine stocastic
+      end
+
+      subroutine batch
+      end
+
       end program derp
